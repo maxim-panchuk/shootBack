04db8fe2f97c0ea6f4e2501c5a78246d2f2eaba7
maksimpancuk <maksim.panchuk29@gmail.com>
2022-02-23 16:19:25 +0300

    fronted continuing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  /*async componentDidMount() {
+    const response = await fetch("/users");
+    const body = await response.json();
+    this.setState({users : body});
+  }*/
+
+  render () {
+    return (
+        <h1>H1</h1>
+    )
+
+    /*const {users} = this.state;
+    return (
+        <div className="App">
+          <h1>Users</h1>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Users</h2>
+              {users.map(user =>
+                    <div key={user.id}>
+                      {user.username} ({user.password})
+                    </div>
+                )}
+            </div>
+          </header>
+        </div>
+    )*/
+  }
 }
-
 export default App;
--- src/main/java/lab/panchuk/application/controller/MainController.java
+++ src/main/java/lab/panchuk/application/controller/MainController.java
@@ -14,9 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
-@RequestMapping()
+@RequestMapping("/users")
 public class MainController {
 
     private final DotServiceImpl dotService;
@@ -29,50 +30,55 @@ public class MainController {
     }
 
     @GetMapping
-    public String index(@ModelAttribute("user") User user) {
-        return "sign_in";
+    public List<User> getUsers() {
+        return userService.getAll();
     }
 
-    @GetMapping("/sign_up")
-    public String getSignUp (@ModelAttribute("user") User user) {
-        return "sign_up";
-    }
-
-    @PostMapping("/sign_in")
-    public String signIn (@ModelAttribute("user") User user, Model model) {
-        String username = user.getUsername();
-        User dbUser = userService.getUserByUsername(username);
-
-        // username doesn't exist
-        if (dbUser == null) {
-            return "sign_in";
-        }
-        String dbPassword = dbUser.getPassword();
-        String password = user.getPassword();
-        String encryptedPassword = SHA384Coder.encryptPassword(password);
-
-        // password doesn't suit
-        if (!dbPassword.equals(encryptedPassword)) {
-            return "sign_in";
-        }
-        user.setPassword(encryptedPassword);
-        model.addAttribute("user", user);
-        return "/main";
-    }
-
-    @PostMapping("/sign_up")
-    public String singUp (@ModelAttribute User user, Model model) {
-        String username = user.getUsername();
-
-        if (userService.getUserByUsername(username) != null) {
-            return "sign_up";
-        }
-
-        String password = user.getPassword();
-        String encryptedPassword = SHA384Coder.encryptPassword(password);
-        user.setPassword(encryptedPassword);
-        model.addAttribute("user", user);
-        userService.addUser(user);
-        return "/main";
-    }
+//    @GetMapping
+//    public String index(@ModelAttribute("user") User user) {
+//        return "sign_in";
+//    }
+//
+//    @GetMapping("/sign_up")
+//    public String getSignUp (@ModelAttribute("user") User user) {
+//        return "sign_up";
+//    }
+//
+//    @PostMapping("/sign_in")
+//    public String signIn (@ModelAttribute("user") User user, Model model) {
+//        String username = user.getUsername();
+//        User dbUser = userService.getUserByUsername(username);
+//
+//        // username doesn't exist
+//        if (dbUser == null) {
+//            return "sign_in";
+//        }
+//        String dbPassword = dbUser.getPassword();
+//        String password = user.getPassword();
+//        String encryptedPassword = SHA384Coder.encryptPassword(password);
+//
+//        // password doesn't suit
+//        if (!dbPassword.equals(encryptedPassword)) {
+//            return "sign_in";
+//        }
+//        user.setPassword(encryptedPassword);
+//        model.addAttribute("user", user);
+//        return "/main";
+//    }
+//
+//    @PostMapping("/sign_up")
+//    public String singUp (@ModelAttribute User user, Model model) {
+//        String username = user.getUsername();
+//
+//        if (userService.getUserByUsername(username) != null) {
+//            return "sign_up";
+//        }
+//
+//        String password = user.getPassword();
+//        String encryptedPassword = SHA384Coder.encryptPassword(password);
+//        user.setPassword(encryptedPassword);
+//        model.addAttribute("user", user);
+//        userService.addUser(user);
+//        return "/main";
+//    }
 }
